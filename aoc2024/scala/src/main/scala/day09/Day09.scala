f4ee00f9054fe10a0e0107a8935514997a1ca68a
Kristina Nylander <kristina.nylander@kvirkla.se>
2024-12-09 23:12:55 +0100

    Solution pt 2 day 9

--- aoc2024/scala/src/main/scala/day09/Day09.scala
+++ aoc2024/scala/src/main/scala/day09/Day09.scala
@@ -3,14 +3,22 @@ package day09
 import scala.annotation.tailrec
 import scala.io.Source
 
+trait Block:
+  val start: Int
+  val length: Int
+
+case class File(start: Int, length: Int, index: Int) extends Block
+
+case class FreeSpace(start: Int, length: Int) extends Block
+
 @tailrec
 def pt1(data: List[Option[Int]], compacted: List[Int] = List()): BigInt =
   if data.isEmpty then checksum(compacted)
   else (data.head, data.last) match
     case (Some(ind), Some(_)) => pt1(data.tail, compacted :+ ind)
-    case (Some(_), None)      => pt1(data.dropRight(1), compacted)
-    case (None, Some(ind))    => pt1(data.tail.dropRight(1), compacted :+ ind)
-    case (None, None)         => pt1(data.dropRight(1), compacted)
+    case (Some(_), None) => pt1(data.dropRight(1), compacted)
+    case (None, Some(ind)) => pt1(data.tail.dropRight(1), compacted :+ ind)
+    case (None, None) => pt1(data.dropRight(1), compacted)
 
 def checksum(data: List[Int]): BigInt =
   data.zipWithIndex.map((num, index) => BigInt(num) * index).sum
@@ -29,26 +37,57 @@ def parseInput(str: String, data: List[Option[Int]] = List(), currentIndex: Int 
     val emptySpaces = (0 until emptySpacesLength).map(_ => Option.empty[Int])
     parseInput(str.tail.tail, data ++ fileBlock ++ emptySpaces, currentIndex + 1)
 
+def pt2(input: String): BigInt =
+  val (files, freeBlocks) = parseInput2(input)
+  val compactedBlocks = compactBlocks(files.reverse, freeBlocks)
+  checksum2(compactedBlocks)
+
+def checksum2(files: List[File]): BigInt =
+  files.flatMap(f =>
+      (f.start until (f.start + f.length))
+        .map(i => i * f.index)
+        .map(BigInt(_)))
+    .sum
+
 @tailrec
-def parseInput2(str: String, data: List[(Int, Option[Int])] = List(), currentIndex: Int = 0): List[(Int, Option[Int])] =
-  if str.isEmpty then data
+def compactBlocks(files: List[File], freeSpace: List[FreeSpace], result: List[File] = List()): List[File] =
+  if files.isEmpty then result
+  else
+    val nextFile = files.head
+    getFreeBlock(nextFile, freeSpace) match
+      case None => compactBlocks(files.tail, freeSpace, result :+ nextFile)
+      case Some((before, free, after)) =>
+        if free.length == nextFile.length then
+          compactBlocks(files.tail, before ++ after, result :+ File(start = free.start, length = nextFile.length, index = nextFile.index))
+        else
+          val newFreeSpace = FreeSpace(start = free.start + nextFile.length, length = free.length - nextFile.length)
+          compactBlocks(files.tail, before ++ List(newFreeSpace) ++ after, result :+ File(start = free.start, length = nextFile.length, index = nextFile.index))
+
+def getFreeBlock(file: File, spaces: List[FreeSpace]): Option[(List[FreeSpace], FreeSpace, List[FreeSpace])] =
+  spaces.find(f => f.length >= file.length && f.start < file.start) match
+    case None => None
+    case Some(free) =>
+      val untilFree = spaces.takeWhile(_ != free)
+      val afterFree = spaces.dropWhile(_ != free).tail
+      Some(untilFree, free, afterFree)
+
+@tailrec
+def parseInput2(str: String, files: List[File] = List(), freeSpace: List[FreeSpace] = List(), fileIndex: Int = 0, currentPos: Int = 0): (List[File], List[FreeSpace]) =
+  if str.isEmpty then (files, freeSpace)
   else if str.length == 1 then
     val fileLength = str.head.asDigit
-    data :+ (fileLength, Option(currentIndex))
+    (files :+ File(start = currentPos, length = fileLength, index = fileIndex), freeSpace)
   else
     val fileLength = str.head.asDigit
-    val fileBlock = (fileLength, Option(currentIndex))
+    val fileBlock = File(start = currentPos, length = fileLength, index = fileIndex)
     val emptySpacesLength = str.tail.head.asDigit
-    val emptySpaces = (emptySpacesLength, Option(currentIndex + 1))
-    parseInput2(str.tail.tail, data :+ fileBlock :+ emptySpaces)
+    val emptyBlock = FreeSpace(start = currentPos + fileLength, length = emptySpacesLength)
+    parseInput2(str.drop(2), files :+ fileBlock, freeSpace :+ emptyBlock, fileIndex + 1, currentPos + fileLength + emptySpacesLength)
 
 @main
 def main(): Unit =
   val input = Source.fromResource("day09.txt").mkString
   val data = parseInput(input)
   println(s"pt1: ${pt1(data)}")
-  println(s"pt2: ${}")
+  println(s"pt2: ${pt2(input)}")
 
-val example = "12345"
-
-val example2 = "2333133121414131402"
