99294f0b4300ae6c2e1bae73152c299a34a25a43
Kristina Nylander <kristina.nylander@kvirkla.se>
2024-12-13 09:03:01 +0100

    Solutions pt 2 day 13

--- aoc2024/scala/src/main/scala/day13/Day13.scala
+++ aoc2024/scala/src/main/scala/day13/Day13.scala
@@ -1,31 +1,48 @@
 package day13
 
-import scala.annotation.tailrec
 import scala.io.Source
-import scala.util.boundary
 
 case class Game(a: Button, b: Button, prize: Prize)
-case class Button(x: Int, y: Int)
-case class Prize(x: Int, y: Int)
+case class Button(x: BigInt, y: BigInt)
+case class Prize(x: BigInt, y: BigInt)
 
-def pt1(data: List[Game]): Int =
-  data.map(getCost(_)).sum
+def pt1(data: List[Game]): BigInt =
+  data.map(getCost).sum
 
-@tailrec
-def getCost(game: Game, i: Int = 0, j: Int = 0): Int =
-  val scoreX = i * game.a.x + j * game.b.x
-  val scoreY = i * game.a.y + j * game.b.y
-  if scoreX == game.prize.x && scoreY == game.prize.y then 3 * i + j
-  else if i > 100 && j > 100 then 0
-  else if scoreX > game.prize.x || scoreY > game.prize.y then getCost(game, i + 1)
-  else if j > 100 then getCost(game, i + 1)
-  else getCost(game, i, j + 1)
+def getCost(game: Game): Int =
+  var cost: Option[Int] = None
+  for i <- 0 to 100 do
+    for j <- 0 to 100 do
+      val scoreX = i * game.a.x + j * game.b.x
+      val scoreY = i * game.a.y + j * game.b.y
+      if scoreX == game.prize.x && scoreY == game.prize.y then
+        cost = Some(3 * i + j)
+  cost.getOrElse(0)
 
+def pt2(data: List[Game]): BigInt =
+  val cost = BigInt("10000000000000")
+  data.map(g => getCost2(Game(g.a, g.b, Prize(g.prize.x + cost, g.prize.y + cost)))).sum
+
+def getCost2(g: Game): BigInt =
+  if noSolution(g) then 0
+  else
+    val detX = g.prize.x * g.b.y - g.prize.y * g.b.x
+    val detY = g.a.x * g.prize.y - g.a.y * g.prize.x
+    val det = g.a.x * g.b.y - g.a.y * g.b.x
+
+    val x = BigDecimal(detX) / BigDecimal(det)
+    val y = BigDecimal(detY) / BigDecimal(det)
+    if x.toBigIntExact.isDefined && y.toBigIntExact.isDefined then
+      3 * x.toBigInt + y.toBigInt
+    else BigInt(0)
+
+def noSolution(game: Game): Boolean =
+  val det = game.a.x * game.b.y - game.a.y * game.b.x
+  det == 0
 
 def parseInput(input: List[String]): List[Game] =
   input.map(_.split("\n").map(_.trim).toList).map {
-    case List(a, b, p) =>
-      Game(parseButton(a), parseButton(b), parsePrize(p))
+    case List(a, b, p) => Game(parseButton(a), parseButton(b), parsePrize(p))
     case _ => throw new IllegalArgumentException("Invalid input")
   }
 
@@ -42,24 +59,4 @@ def main(): Unit =
   val input = Source.fromResource("day13.txt").mkString.split("\n\n").map(_.trim).toList
   val data = parseInput(input)
   println(s"pt1: ${pt1(data)}")
-  println(s"pt2: ${}")
-
-
-val example =
-  """
-    |Button A: X+94, Y+34
-    |Button B: X+22, Y+67
-    |Prize: X=8400, Y=5400
-    |
-    |Button A: X+26, Y+66
-    |Button B: X+67, Y+21
-    |Prize: X=12748, Y=12176
-    |
-    |Button A: X+17, Y+86
-    |Button B: X+84, Y+37
-    |Prize: X=7870, Y=6450
-    |
-    |Button A: X+69, Y+23
-    |Button B: X+27, Y+71
-    |Prize: X=18641, Y=10279
-    |""".stripMargin.split("\n\n").map(_.trim).filterNot(_.isBlank).toList
+  println(s"pt2: ${pt2(data)}")
