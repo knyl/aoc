d9640797445ef1d1abfae167811473edac5162be
Kristina Nylander <kristina.nylander@kvirkla.se>
2024-12-25 07:57:21 +0100

    Solution pt 1 day 25

--- aoc2024/scala/src/main/scala/day25/Day25.scala
+++ aoc2024/scala/src/main/scala/day25/Day25.scala
@@ -1,50 +1,35 @@
-package day04
-
-import util.{Map2D, Pos, parseMap}
+package day25
 
 import scala.io.Source
 
-def pattern1(x: Int, y: Int) = List(
-  List((x + 1, y), (x + 2, y), (x + 3, y)),
-  List((x + 1, y - 1), (x + 2, y - 2), (x + 3, y - 3)),
-  List((x, y - 1), (x, y - 2), (x, y - 3)),
-  List((x - 1, y - 1), (x - 2, y - 2), (x - 3, y - 3)),
-  List((x - 1, y), (x - 2, y), (x - 3, y)),
-  List((x - 1, y + 1), (x - 2, y + 2), (x - 3, y + 3)),
-  List((x, y + 1), (x, y + 2), (x, y + 3)),
-  List((x + 1, y + 1), (x + 2, y + 2), (x + 3, y + 3))
-)
+def pt1(locks: List[List[Int]], keys: List[List[Int]]): Int =
+  locks.map(lock =>
+    keys.count(key => open(lock, key))
+  ).sum
 
-def matchPt1(pattern: String): Boolean = pattern match
-  case "MAS" => true
-  case _ => false
+def open(lock: List[Int], key: List[Int]): Boolean =
+  lock.zip(key).forall((l, k) => l + k <= 5)
 
-def pattern2(x: Int, y: Int) = List(
-  List((x + 1, y - 1), (x - 1, y - 1), (x - 1, y + 1), (x + 1, y + 1))
-)
+def parseObject(str: String) =
+  str
+    .split("\n")
+    .tail
+    .dropRight(1)
+    .toList
+    .transpose
+    .map(l => l.count(_ == '#'))
 
-def matchPt2(pattern: String): Boolean = pattern match
-  case "MMSS" => true
-  case "SMMS" => true
-  case "SSMM" => true
-  case "MSSM" => true
-  case _ => false
-
-def solve(data: Map2D[Char], mainChar: Char, pattern: (Int, Int) => List[List[(Int, Int)]], stringMatch: String => Boolean): Int =
-  data.map.map {
-    case (Pos(x, y), c) if c == mainChar =>
-      pattern(x, y)
-        .map(p => p.map(f => data(Pos(f._1, f._2))).mkString)
-        .count(stringMatch)
-    case _ => 0
-  }.sum
-
-def parseInput(line: List[String]): Map2D[Char] =
-  parseMap(line, identity, '.')
+def parseInput(input: List[String]): (List[List[Int]], List[List[Int]]) =
+  val locks = input
+    .filter(s => s.startsWith("#####") && s.endsWith("....."))
+    .map(parseObject)
+  val keys = input
+    .filter(s => s.startsWith(".....") && s.endsWith("#####"))
+    .map(parseObject)
+  (locks, keys)
 
 @main
 def main(): Unit =
-  val input = Source.fromResource("day04.txt").getLines().toList
-  val data = parseInput(input)
-  println(s"pt1: ${solve(data, 'X', pattern1, matchPt1)}")
-  println(s"pt2: ${solve(data, 'A', pattern2, matchPt2)}")
+  val input = Source.fromResource("day25.txt").mkString.split("\n\n").map(_.trim).toList
+  val (locks, keys) = parseInput(input)
+  println(s"pt1: ${pt1(locks, keys)}")
